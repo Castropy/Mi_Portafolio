130dde4be3110c32229f1ee1ee806643ea40e560
Castropy <randrescastroi@gmail.com>
2026-01-23 12:41:43 -0400

    Import RouterModule from home.component

--- src/app/components/home.component/home.component.ts
+++ src/app/components/home.component/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-home.component',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
