cf51dae5a26690b9cc19ffea5e0d756b2dd22ea4
Castropy <randrescastroi@gmail.com>
2026-01-24 10:04:55 -0400

    Import RouterModule From academic.component

--- src/app/components/academic.component/academic.component.ts
+++ src/app/components/academic.component/academic.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-academic',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './academic.component.html',
   styleUrl: './academic.component.scss',
 })
