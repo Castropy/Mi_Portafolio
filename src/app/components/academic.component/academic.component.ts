38e0b5218b5e62babab2ba6acfcb22b3787c9f71
Castropy <randrescastroi@gmail.com>
2026-01-24 11:00:35 -0400

    Import DataService from academic.components

--- src/app/components/academic.component/academic.component.ts
+++ src/app/components/academic.component/academic.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-academic',
