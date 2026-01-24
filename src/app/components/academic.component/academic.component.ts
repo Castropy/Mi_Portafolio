68a33748c09a3c066a1156f31ac0111cf2f7c9d4
Castropy <randrescastroi@gmail.com>
2026-01-24 11:36:23 -0400

    Import correcto: inject

--- src/app/components/academic.component/academic.component.ts
+++ src/app/components/academic.component/academic.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { DataService } from '../../services/data.service';
@@ -12,7 +12,7 @@ import { DataService } from '../../services/data.service';
 })
 export class Academic {
 // Acceso al cerebro de datos
-  private dataService = Inject(DataService);
+  private dataService = inject(DataService);
   
   // Exponemos la señal a la vista
   public info = this.dataService.academicData;
