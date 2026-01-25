f1e212db495fe367c4b26ad723b518d7b14bebb4
Castropy <randrescastroi@gmail.com>
2026-01-25 17:22:28 -0400

    Import inject, CommonModule and class DataService from collaborations.component

--- src/app/components/collaborations.component/collaborations.component.ts
+++ src/app/components/collaborations.component/collaborations.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-collaborations',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './collaborations.component.html',
   styleUrl: './collaborations.component.scss',
 })
