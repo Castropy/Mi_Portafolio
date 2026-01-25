9ee1e4b4f0caddf9a6b0753cf9fca04d6a3f69c9
Castropy <randrescastroi@gmail.com>
2026-01-25 17:05:24 -0400

    import class dataService from experience.component

--- src/app/components/experience.component/experience.component.ts
+++ src/app/components/experience.component/experience.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { DataService } from '../../services/data.service';
 @Component({
   selector: 'app-experience',
   standalone: true,
