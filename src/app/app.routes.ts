2db487f69b1e61a21ee74b1be05d0957aec52f2d
Castropy <randrescastroi@gmail.com>
2026-03-17 17:57:22 -0400

    feat(route): Privaty policy into app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home.component/home.component';
+import { PrivacyPolicy } from './components/privacy-policy.component/privacy-policy.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent } // Ruta raíz (vacia) carga el Home
+  { path: '', component: HomeComponent }, // Ruta raíz (vacia) carga el Home
+  { path: 'privacy-policy', component: PrivacyPolicy  } //Ruta para la política de privacidad de recetas app
 ];
